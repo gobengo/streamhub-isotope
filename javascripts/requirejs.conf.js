0e01701ca87d1c3497bef8632e52653a908d2413
Benjamin Goering <bengoering@gmail.com>
2013-02-11 11:19:02 -0800

    Add live example

---
+++ javascripts/requirejs.conf.js
@@ -0,0 +1,48 @@
+require.config({
+    urlArgs: 'cb=' + Math.random(),
+    baseUrl: 'javascripts',
+    packages: [{
+        name: 'streamhub-backbone',
+        location: 'lib/streamhub-backbone'
+    },{
+        name: 'streamhub-isotope',
+        location: 'lib/streamhub-isotope'
+    }],
+    paths: {
+        jquery: 'lib/jquery/jquery',
+        underscore: 'lib/underscore/underscore',
+        backbone: 'lib/backbone/backbone',
+        mustache: 'lib/mustache/mustache',
+        isotope: 'lib/isotope/jquery.isotope',
+        text: 'lib/requirejs-text/text',
+        jasmine: 'lib/jasmine/lib/jasmine-core/jasmine',
+        'jasmine-html': 'lib/jasmine/lib/jasmine-core/jasmine-html',
+        'jasmine-jquery': 'lib/jasmine-jquery/lib/jasmine-jquery',
+        fyre: 'http://zor.livefyre.com/wjs/v3.0/javascripts/livefyre'
+    },
+    shim: {
+        jasmine: {
+            exports: 'jasmine'
+        },
+        'jasmine-html': {
+            deps: ['jasmine'],
+            exports: 'jasmine'
+        },
+        'jasmine-jquery': {
+            deps: ['jquery', 'jasmine']
+        },
+        fyre: {
+            exports: 'fyre'
+        },
+        backbone: {
+            deps: ['underscore', 'jquery'],
+            exports: 'Backbone'
+        },
+        isotope: {
+            deps: ['jquery']
+        },
+        underscore: {
+            exports: '_'
+        }
+    }
+});
