7345638e64f900f732e1ee171e2cab2621f2e220
Benjamin Goering <bengoering@gmail.com>
2013-02-11 22:33:04 -0800

    Add docs to examples/base. Link to git URL for backbone dep

--- examples/base/main.js
+++ examples/base/main.js
@@ -1,29 +1,44 @@
-require(['requirejs.conf'], function () {
-require([
-	'fyre',
-	'streamhub-backbone',
-	'streamhub-isotope'],
-function (fyre, Hub, IsotopeView) {
-	var Config = {
-		el: document.getElementById('hub-media-wall'),
-		network: 'livefyre.com',
-		siteId: 303818,
-		articleId: '172'
-	};
+// # StreamHub Media Wall Example
 
-	fyre.conv.load({
-		network: Config.network
-	}, [{ app: 'sdk'}], function (sdk) {
-		var collection = new Hub.Collection(),
-			view = new IsotopeView({
-				el: Config.el,
-				collection: collection
-			}).render();
-		collection.setRemote({
-			sdk: sdk,
-			siteId: Config.siteId,
-			articleId: Config.articleId
-		});
-	});
+// Require the config file that tells requirejs where dependency packages are at
+require(['requirejs.conf'], function () {
+
+// Require the dependencies
+//
+// * `fyre` - The Livefyre JavaScript API
+// * `streamhub-backbone` - provides the Collection model
+// * `streamhub-isotope` - provides an IsotopeView to lay out Content in the Collection
+require([
+    'fyre',
+    'streamhub-backbone',
+    'streamhub-isotope'],
+function (fyre, Hub, IsotopeView) {
+    // keep track of Config separate from instantiation logic
+    var Config = {
+        el: document.getElementById('hub-media-wall'),
+        network: 'livefyre.com',
+        siteId: 303818,
+        articleId: '172'
+    };
+    // Load the Livefyre JavaScript SDK
+    fyre.conv.load({
+        network: Config.network
+    }, [{ app: 'sdk'}], function (sdk) {
+        // Create the Collection that will store the Content
+        var collection = new Hub.Collection(),
+        // Create the IsotopeView bound to this Collection
+            view = new IsotopeView({
+                el: Config.el,
+                collection: collection
+            });
+        // be sure to render the view so it is visible
+        view.render();
+        // Bind the Collection to a remote Collection in StreamHub
+        collection.setRemote({
+            sdk: sdk,
+            siteId: Config.siteId,
+            articleId: Config.articleId
+        });
+    });
 });
 });
